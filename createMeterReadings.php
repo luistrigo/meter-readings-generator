01008d29eb7e9485ae3859dd8206c7505fbf8e49
luistrigoc <luistrigoc@hotmail.com>
2022-01-02 10:50:30 +0100

    :bug: Error calculating dates
    Now starts in today less X years

--- createMeterReadings.php
+++ createMeterReadings.php
@@ -25,47 +25,37 @@ $customersEnd = $lastCustomers + $nCustomers;
 $sql = "SELECT id,meters FROM customers WHERE id >='$lastCustomers' and id <=$customersEnd ORDER BY id";
 $r_customers = mysqli_query($connection, $sql);
 $t_customers = mysqli_num_rows($r_customers);
-$ster_year = date("Y") - $year;
-
-$start_month = 1;
-
-
+$today = new DateTime('now');
+$now = new DateTime('now');
+$now = $now->modify('+1 DAY');
 
 if ($t_customers > 0) {
     $counter = 0;
     while ($row_customers = mysqli_fetch_assoc($r_customers)) {
         //meterReadings for each customer
+        $newtime = $today->modify('-'.$year.' YEAR');
+        if ($meterReading_type == 1) {
+            //Monthly First month day
+            $newtime = new DateTime($newtime->format("Y")."-".$newtime->format("m").'-01');
+        }
         $counterKW = 0;
-        for ($i = 0; $i < $year; $i++) {
+        while($newtime->format('U') < $now->format('U')){
             //KW ramdom min 50 max 50 + meters
             $KW = rand(50, $row_customers['meters'] + 50);
-
-            for ($j = 1; $j < 13; $j++) {
-                if ($meterReading_type == 0) {
-                    //Daily
-                    $month = $j + 1; //next month
-                    if ($month == 12)
-                        $month = 1; //january have 31 days
-
-                    $date = mktime('0', '0', '0', $month, 1, date("Y") - $i);
-                    $date = $date - 86400; //day 1 next month less 1 day (24h*60m*60s) is last day previous month
-                    $daymonth = date("d", $date);
-                } else {
-                    //Monthly
-                    $daymonth = 1;
-                }
-                for ($k = 1; $k < ($daymonth + 1); $k++) {
-                    $dateMeterReading = date("Y") - $i . "-" . $j . "-" . $k;
-                    $KWmas = rand(5, 20);
-                    $KW_T = $KW + ($KW * $KWmas / 100); //between 5% - 20%
-                    $counterKW += $KW_T;
-                    $id_customers = $row_customers['id'];
-                    $sql = "insert into meter_readings (id_customer,kw,datetime,reading) values('$id_customers','$KW_T','$dateMeterReading','$counterKW')";
-                    $newMeterReading = mysqli_query($connection, $sql);
-
-                    $counter++;
-                }
-                
+            $dateMeterReading = date("Y") - $i . "-" . $j . "-" . $k;
+            $KWmas = rand(5, 20);
+            $KW_T = $KW + ($KW * $KWmas / 100); //between 5% - 20%
+            $counterKW += $KW_T;
+            $id_customers = $row_customers['id'];
+            $sql = "insert into meter_readings (id_customer,kw,datetime,reading) values('".$id_customers."','".$KW_T."','".$newtime->format('Y-m-d')."','".$counterKW."')";
+            $newMeterReading = mysqli_query($connection, $sql);
+            $counter++;
+            if ($meterReading_type == 0) {
+                //Daily
+                $newtime = $newtime->modify('+1 DAY');
+            } else {
+                //Monthly
+                $newtime = $newtime->modify('+1 MONTH');
             }
         }
     }
